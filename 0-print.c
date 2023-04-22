0aaf23e96f539e3d722681ea7fcf786b5671ef85
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-04-22 05:17:21 -0700

    C source file for printf function

---
+++ 0-print.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+#include <stdarg.h>
+
+/**
+ * _printf - print the output of a formatted string
+ * @format: The format string
+ * Return: The total number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int i, count = 0, c;
+	char per = '%', *str;
+	va_list arg_list;
+
+	if (format == NULL)
+		return (count);
+	va_start(arg_list, format);
+	for (i = 0; format[i] > '\0'; i++)
+	{
+		if (!(format[i] == '%'))
+		{
+			write(1, &format[i], 1);
+			count += 1;
+			continue;
+		}
+		switch (format[i + 1])
+		{
+		case 'c':
+			c = va_arg(arg_list, int);
+			write(1, &c, 1);
+			break;
+		case 's':
+			str = va_arg(arg_list, char *);
+			if (str)
+				write(1, str, strlen(str));
+			break;
+		case '%':
+			write(1, &per, 1);
+			break;
+		}
+		i += 1;
+		count += 1;
+	}
+	return (count);
+}
