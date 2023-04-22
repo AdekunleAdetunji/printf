b84c772d836d307ec4f36e7feb5f2f2aa1a0fe4c
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-04-22 11:02:22 -0700

    Modified the loop condition

--- 0_printf.c
+++ 0_printf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (count);
 	va_start(arg_list, format);
-	for (i = 0; format[i] > '\0'; i++)
+	for (i = 0; !(format[i] == '\0'); i++)
 	{
 		if (!(format[i] == '%'))
 		{
