9e1a0d4e35955866d5078f60f547306cecb1d1da
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-04-22 09:53:38 -0700

    printf_task 0

--- _printf.c
+++ _printf.c
@@ -42,7 +42,7 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	if (format == NULL)
 		return (-1);
-	for (i = 0; format != NULL && format[i] != '\0'; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
