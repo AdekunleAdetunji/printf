bae9ac9075c3de7247c27ddf83df534d3e80b0e1
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-04-22 18:32:06 -0700

    introduced 2 functions

--- _printf.c
+++ _printf.c
@@ -1,49 +1,88 @@
 #include "main.h"
 
 /**
+ * print_buffer - print the contents of a buffer and reset the buffer index
+ * @b: the buffer
+ * @buff_ind: a pointer to the buffer index
+ *
+ * Return: the number of characters printed
+ */
+void print_buffer(char b[], int *buff_ind)
+{
+	write(1, b, *buff_ind);
+	*buff_ind = 0;
+}
+
+/**
+ * _format_specifier - handle a format specifier
+ * @b: the buffer
+ * @buffer_pos: a pointer to the buffer position
+ * @args: argument list
+ * @format: the format specifier character
+ *
+ * Return: void
+ */
+void _format_specifier(char b[], int *buffer_pos, va_list args, char format)
+{
+	int len;
+	char *str;
+
+	switch (format)
+	{
+		case 'c':
+			b[*buffer_pos] = va_arg(args, int);
+			(*buffer_pos)++;
+			break;
+		case 's':
+			str = va_arg(args, char *);
+			len = strlen(str);
+			if (*buffer_pos + len > BUFF_SIZE - 1)
+				print_buffer(b, buffer_pos);
+			memcpy(b + *buffer_pos, str, len);
+			*buffer_pos += len;
+			break;
+		case '%':
+			b[*buffer_pos] = '%';
+			(*buffer_pos)++;
+			break;
+		default:
+			b[*buffer_pos] = '%';
+			(*buffer_pos)++;
+			b[*buffer_pos] = format;
+			(*buffer_pos)++;
+			break;
+	}
+}
+
+/**
  * _printf - print the output of a formatted string
  * @format: The format string
+ *
  * Return: The total number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int i, count = 0, c;
-	char per = '%', *str;
-	va_list arg_list;
+	va_list args;
+	int i, count = 0, buffer_pos = 0;
+	char b[BUFF_SIZE];
 
-	if (format == NULL)
-		return (-1);
-	va_start(arg_list, format);
-	for (i = 0; !(format[i] == '\0'); i++)
+	va_start(args, format);
+	for (i = 0; format && format[i]; i++)
 	{
-		if (!(format[i] == '%'))
+		if (format[i] == '%')
 		{
-			write(1, &format[i], 1), count += 1;
-			continue;
+			i++;
+			_format_specifier(b, &buffer_pos, args, format[i]);
 		}
-		switch (format[i + 1])
+		else
 		{
-		case 'c':
-			c = va_arg(arg_list, int), write(1, &c, 1);
-			i += 1;
-			break;
-		case 's':
-			str = va_arg(arg_list, char *);
-			if (str)
-			{
-				write(1, str, strlen(str));
-				i += 1;
-			}
-			break;
-		case '%':
-			write(1, &per, 1), i += 1;
-			break;
-		default:
-			write(1, &per, 1);
-			break;
+			b[buffer_pos] = format[i];
+			buffer_pos++;
+			if (buffer_pos == BUFF_SIZE - 1)
+				print_buffer(b, &buffer_pos);
 		}
-		count += 1;
 	}
-	va_end(arg_list);
+	print_buffer(b, &buffer_pos);
+	va_end(args);
 	return (count);
 }
--- main.h
+++ main.h
@@ -5,8 +5,10 @@
 #include <stdarg.h>
 #include <string.h>
 
+void print_buffer(char b[], int *buff_ind);
 int _printf(const char *format, ...);
 int _strlen_recursion(char *s);
 int _write_char(char c);
-
+void _format_specifier(char b[], int *buffer_pos, va_list args, char format);
+#define BUFF_SIZE 1024
 #endif
