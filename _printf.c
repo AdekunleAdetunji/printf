8fef6af992510eb17b69c590ce4313e9bf7b8708
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-04-23 02:33:53 -0700

    Created a new funtion in _printf file

--- _printf.c
+++ _printf.c
@@ -1,7 +1,44 @@
 #include "main.h"
-#include <unistd.h>
-#include <stdarg.h>
-#include <string.h>
+
+/**
+ * char_str - replaces %c, %s, %, flag in code block
+ * @format: The character string under consideration
+ * @arg_list: The variadic argument list
+ * @i: The i value we are iterating with
+ * @count: The count of the values printed to stdout
+ * Return: Void values
+ */
+void char_str(const char *format, va_list arg_list, int *i, int *count)
+{
+	char *str, c, per = '%';
+
+	switch (format[*i + 1])
+		{
+		case 'c':
+			c = va_arg(arg_list, int);
+			if (c)
+			{
+				write(1, &c, 1);
+				*i += 1;
+			}
+			break;
+		case 's':
+			str = va_arg(arg_list, char *);
+			if (str)
+			{
+				write(1, str, strlen(str));
+				*i += 1;
+			}
+			break;
+		case '%':
+			write(1, &per, 1), *i += 1;
+			break;
+		default:
+			write(1, &per, 1);
+			break;
+		}
+	*count += 1;
+}
 
 /**
  * _printf - print the output of a formatted string
@@ -10,42 +47,20 @@
  */
 int _printf(const char *format, ...)
 {
-	int i, count = 0, c;
-	char per = '%', *str;
+	int i, count = 0;
 	va_list arg_list;
 
 	if (format == NULL)
-		return (-1);
+		return (count);
 	va_start(arg_list, format);
-	for (i = 0; !(format[i] == '\0'); i++)
+	for (i = 0; format[i] > '\0'; i++)
 	{
 		if (!(format[i] == '%'))
 		{
 			write(1, &format[i], 1), count += 1;
 			continue;
 		}
-		switch (format[i + 1])
-		{
-		case 'c':
-			c = va_arg(arg_list, int), write(1, &c, 1);
-			i += 1;
-			break;
-		case 's':
-			str = va_arg(arg_list, char *);
-			if (str)
-			{
-				write(1, str, strlen(str));
-				i += 1;
-			}
-			break;
-		case '%':
-			write(1, &per, 1), i += 1;
-			break;
-		default:
-			write(1, &per, 1);
-			break;
-		}
-		count += 1;
+		char_str(format, arg_list, &i, &count);
 	}
 	return (count);
 }
--- main.h
+++ main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
 
 int _printf(const char *format, ...);
 int _strlen_recursion(char *s);
