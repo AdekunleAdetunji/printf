8b6e1ebfde2254278fb2cd6d151c544979def4ec
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-04-24 08:46:20 -0700

    Task 1

--- main.h
+++ main.h
@@ -4,6 +4,11 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
+void int_str(va_list arg_list, int *i, int *count);
+char *_itoa(int num);
+char *_reverse(char *str);
+void print_integer(int n);
 int _printf(const char *format, ...);
 #endif
